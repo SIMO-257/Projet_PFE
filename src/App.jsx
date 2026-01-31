27f0c541e86170c103eea8bfe7f79eb1555ebc7d
Mohammed Mammah <MohammedMammah@gmail.com>
2026-01-31 21:48:36 +0100

    Fix import paths in App.jsx to match src/Pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/SignUp/SignUp";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import HomeScreen from "./Pages/Home/HomeScreen";
-// import WalletApp from "./Pages/Home/Home";
-import ValidationScreen from "./Pages/ValidationScreen/ValidatinScreen";
-import QRValidationModal from "./Pages/QRValidationModule/QRValidationModal";
-import NFCValidationModal from "./Pages/ValidationNFC/ValidationNFC";
-import ValidationResultModal from "./Pages/ValidationResultModal/ValidationResultModal";
+import ValidationScreen from "./Pages/ValidationScreen/ValidationScreen";
+import QRValidationScreen from "./Pages/QRValidationScreen/QRValidationScreen";
+import NFCValidationScreen from "./Pages/NFCValidationScreen/NFCValidationScreen";
+import ValidationResultScreen from "./Pages/ValidationResultScreen/ValidationResultScreen";
 
-//import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
